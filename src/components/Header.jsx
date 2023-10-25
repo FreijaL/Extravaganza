ca7f44cfdbd46c026ec5b78c87bf4f369ca1376d
FreijaL <110614499+FreijaL@users.noreply.github.com>
2023-10-25 13:50:03 +0200

    work on homepage design

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+// import style from '../pages/HomePage.scss';
+
+// function Header() {
+//     <header className="homepage__header">
+//         <img id="extravaganza-logo" src="/svg/logo.svg" alt="extravaganza-logo" />
+//         <h1 className="homepage__heading">EXTRAVAGANZA</h1>
+//         <img id="shopping-cart" src="/svg/shopping-cart.svg" alt="shopping-cart-icon" />
+//     </header>
+// }
+
+// export default Header;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+//import Header from '../components/Header'
 import style from './homepage.scss';
 
 
@@ -7,9 +8,44 @@ function HomePage() {
     const navigate = useNavigate();
 
     return (
-        <main>
-            <h1 className="homepage__heading">testing homepage</h1>
-        </main>
+        <>
+            <article className="homepage__wrapper">
+                <header className="homepage__header">
+                    <img id="extravaganza-logo" src="/svg/logo.svg" alt="extravaganza-logo" />
+                    <h1 className="homepage__heading">EXTRAVAGANZA</h1>
+                    <img id="shopping-cart" src="/svg/shopping-cart.svg" alt="shopping-cart-icon" />
+                </header>
+                
+                <main className="homepage__main">
+                    <section className="homepage__first-section">
+                        <img id="homepage__first-section-image" src="/images/karusell.jpg" alt="karusell med hästar" />
+                        <h2 className="homepage__first-section-text">Välkommen till en magisk upplevelse för hela familjen!</h2>
+                    </section>
+                    <section className="homepage__news">
+                        <section>
+                            <h2>News in the park</h2>
+                        </section>
+                        <section className="homepage__news-section">
+                            <section className="homepage__news-section-option">
+                                <img className="homepage__news-section-image" src="/images/julmarknad.jpg" alt="julmarknad på nöjesfältet" />
+                            </section>
+                            <section className="homepage__news-section-option">
+                                <img className="homepage__news-section-image" src="/images/jul-slott.jpg" alt="slottet i juldekoration" />
+                            </section>
+                            <section className="homepage__news-section-option">
+                                <img className="homepage__news-section-image" src="/images/fyrverkerier.jpg" alt="fyrverkerier" />
+                            </section>
+                        </section>
+                    </section>
+                </main>
+
+                <footer className="homepage__footer">
+                        <p>Contact: freijalindkvist@hotmail.com</p>
+                        <p>&copy; Copyrights 2023 by Freija Lindkvist </p>
+                        <p>Examination UX/UI</p>
+                </footer>
+            </article>
+        </>
     )
 };
 
